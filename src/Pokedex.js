062e53beec137790e8298be2cebe2afbbb9036ca
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-04-02 23:02:01 -0400

    finished card layout with subtle animations

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -3,23 +3,70 @@ import styled from "styled-components";
 
 import Pokecard from "./Pokecard";
 
-const pokeInfo = [
-  { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
-  { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
-  { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
-  { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
-  { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
-  { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
-  { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
-  { id: 133, name: "Eevee", type: "normal", base_experience: 65 }
-];
-
 export default class Pokedex extends Component {
+  static defaultProps = [
+    {
+      id: 4,
+      name: "Charmander",
+      type: "fire",
+      base_experience: 62
+    },
+    {
+      id: 7,
+      name: "Squirtle",
+      type: "water",
+      base_experience: 63
+    },
+    {
+      id: 11,
+      name: "Metapod",
+      type: "bug",
+      base_experience: 72
+    },
+    {
+      id: 12,
+      name: "Butterfree",
+      type: "flying",
+      base_experience: 178
+    },
+    {
+      id: 25,
+      name: "Pikachu",
+      type: "electric",
+      base_experience: 112
+    },
+    {
+      id: 39,
+      name: "Jigglypuff",
+      type: "normal",
+      base_experience: 95
+    },
+    {
+      id: 94,
+      name: "Gengar",
+      type: "poison",
+      base_experience: 225
+    },
+    {
+      id: 133,
+      name: "Eevee",
+      type: "normal",
+      base_experience: 65
+    }
+  ];
+
   render() {
-    return (
-      <div>
-        <p>{console.log(pokeInfo)}</p>
-      </div>
-    );
+    let pokes = this.props;
+    let pokeInfo = Object.keys(pokes).map(poke => (
+      <Pokecard key={pokes[poke].id} pokeInfo={pokes[poke]} />
+    ));
+    return <PokedexContainer>{pokeInfo}</PokedexContainer>;
   }
 }
+
+const PokedexContainer = styled.section`
+  background: #f1efef;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
